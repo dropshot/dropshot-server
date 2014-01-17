d2ce3dc4b9ad5253cca1287d57dc78d852e1021a
Reilly Tucker Siemens <reilly.siemens@gmail.com>
2014-01-16 19:02:16 -0800

    Outline basic API in dropshot.py. Add vim and sqlite sections to .gitignore.

--- dropshot.py
+++ dropshot.py
@@ -1,10 +1,58 @@
-from bottle import route, run, template
-import models
+#!/usr/bin/env python
+from bottle import route, get, post, run, template
+#import models
 
-@route('/')
-def hello():
-    return "Wassup, yo?"
+# ---- GET REQUESTS -----------------------------------------------------------
 
-@route('/db')
-def create_db():
-    return models.session.query(models.Player).all()[0].to_json()
+@get('/')
+def home():
+    return "dropshot is online"
+
+@get('/ping')
+def pong():
+    return "pong"
+
+@get('/players')
+def get_players():
+    return "No players."
+
+@get('/players/<username>')
+def get_players_by_username(username):
+    return template('No player <b>{{username}}</b>.', username=username)
+
+@get('/players/<username>/games')
+def get_games_by_username(username):
+    return template('No games associated with player <b>{{username}}</b>.', username=username)
+
+@get('/games/<game_id>')
+def get_games_by_id(game_id):
+    return template('No game with ID=<b>{{game_id}}</b>.', game_id=game_id)
+
+@get('/games')
+def get_games():
+    return "No games."
+
+@get('/logout')
+def logout():
+    return "Cannot logout."
+
+# ---- POST REQUESTS ----------------------------------------------------------
+
+@post('/games')
+def post_games():
+    return "Cannot create game."
+
+@post('/players')
+def post_players():
+    return "Cannot create player."
+
+@post('/login')
+def login():
+    return "Cannot login."
+
+#@route('/db')
+#def create_db():
+#    return models.session.query(models.Player).all()[0].to_json()
+
+if __name__ == '__main__':
+    run(host='localhost', port='3000')
