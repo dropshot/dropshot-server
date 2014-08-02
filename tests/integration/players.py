10edfd6ed5058b31a632755d110b3be5226cfe2c
Reilly Tucker Siemens <reilly.siemens@gmail.com>
2014-08-01 17:07:32 -0700

    Make tests/integration/players.py more compatible with PEP 8 guidelines.

--- tests/integration/players.py
+++ tests/integration/players.py
@@ -2,18 +2,20 @@
 import urllib.parse
 import urllib.request
 
+
 def create_player(username, password, email):
     url = 'http://localhost:3000/players'
-    values = {'username' : username,
-              'password' : password,
-              'email'    : email }
+    values = {'username': username,
+              'password': password,
+              'email': email}
 
     data = urllib.parse.urlencode(values)
-    data = data.encode('utf-8') # data should be bytes
+    data = data.encode('utf-8')  # data should be bytes
     req = urllib.request.Request(url, data)
     response = urllib.request.urlopen(req)
     the_page = response.read()
-    print("Created user \'{}\' with password \'{}\' and email \'{}\'".format(username, password, email))
+    print("Created user \'{}\' with password \'{}\' and email \'{}\'".format(
+        username, password, email))
 
 if __name__ == '__main__':
     create_player("chapmang", "password", "chapmang@dropshot.com")
