dda36813ae3e0702f7ca6071c2981702f50e54f4
Reilly Tucker Siemens <reilly.siemens@gmail.com>
2014-08-03 14:41:28 -0700

    Make changes from #32 PEP 8 compliant.

--- tests/integration/players.py
+++ tests/integration/players.py
@@ -5,18 +5,17 @@ import requests
 
 def create_player(username, password, email):
     url = 'https://localhost:3000/players'
-    values = {'username' : username,
-              'password' : password,
-              'email'    : email }
+    values = {'username': username,
+              'password': password,
+              'email': email}
 
     r = requests.post(url, params=values, verify=False)
 
     r.raise_for_status()
 
     if (r.status_code == 201):
-        print("Created user \'{}\' with password \'{}\' and email \'{}\'".format(username,
-                                                                                 password,
-                                                                                 email))
+        print("Created user '{}' with password '{}' and email '{}'".format(
+            username, password, email))
 
 
 if __name__ == '__main__':
