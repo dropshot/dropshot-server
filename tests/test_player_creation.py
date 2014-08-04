05d498cc1f216ba722ce887b212ac5e750fb0c8d
Reilly Tucker Siemens <reilly.siemens@gmail.com>
2014-08-04 16:44:43 -0700

    Make player creation test check for valid response.

--- tests/test_player_creation.py
+++ tests/test_player_creation.py
@@ -1,14 +1,25 @@
 from webtest import TestApp
 import dropshot
 
+
 def test_create_player():
     app = TestApp(dropshot.app)
+
     params = {'username': 'chapmang',
               'password': 'deadparrot',
               'email': 'chapmang@dropshot.com'}
+
+    expected = {'count': 1,
+                'offset': 0,
+                'players': [
+                    {'gamesPlayed': 0,
+                     'username': 'chapmang'}
+                ]}
 
     app.post('/players', params)
 
     res = app.get('/players')
 
     assert res.status_int == 200
+    assert res.content_type == 'application/json'
+    assert res.json == expected
